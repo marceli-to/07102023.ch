3c7f8d2c900c572e9f7794d3367344844bce0444
Marcel Stadelmann <m@marceli.to>
2023-09-01 08:51:54 +0200

    wip

--- resources/views/web/pages/home.blade.php
+++ resources/views/web/pages/home.blade.php
@@ -1,7 +1,7 @@
 @extends('web.app')
 @section('content')
 <div 
-  class="text-white font-bold text-md md:text-lg lg:text-xl leading-none w-[288px] md:w-[450px] lg:w-[900px] text-left"
+  class="text-white font-bold text-md md:text-lg lg:text-xl leading-none w-[292px] md:w-[450px] lg:w-[900px] text-left"
   data-counter-wrapper>
   <span>BF&thinsp;/&thinsp;45:</span>
   <span class="font-semibold" data-counter>36:23:45:59</span>

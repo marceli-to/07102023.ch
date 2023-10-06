2261db56cb9bfa71a2904433e35106970e1f1a71
Marcel Stadelmann <m@marceli.to>
2023-10-06 15:48:06 +0200

    wip

--- resources/js/modules/counter.js
+++ resources/js/modules/counter.js
@@ -13,6 +13,18 @@
   const updateCounter = () => {
     // calculate the difference between current timestamp and the timestamp for the 7th of October 2023 at 7pm
     const difference = Date.parse('2023-10-07T19:00:00') - Date.now();
+
+    // get the counter element
+    const counter = document.querySelector(selectors.counter);
+
+    if (difference <= 0) {
+      counter.textContent = `0:00:00:00`;
+      const wrapper = document.querySelector(selectors.wrapper);
+      wrapper.classList.remove('is-hidden');
+      return;
+    }
+
+
     // calculate the number of days, hours, minutes and seconds from the difference
     let days = Math.floor(difference / (1000 * 60 * 60 * 24));
     let hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
@@ -31,9 +43,6 @@
     if (seconds < 10) {
       seconds = `0${seconds}`;
     }
-
-    // get the counter element
-    const counter = document.querySelector(selectors.counter);
 
     // set the counter text
     counter.textContent = `${days}:${hours}:${minutes}:${seconds}`;
